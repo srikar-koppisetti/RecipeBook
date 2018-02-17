95e118b9044fe5e41a8383c82258d78a243c75d0
srikarslash <srikar.slash@gmail.com>
2018-02-17 10:50:26 -0500

    add components

--- Recipe-Book/src/app/app.module.ts
+++ Recipe-Book/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
